03e6dd7fce248413b830edb7a7abff2f4b337d31
habupain <cryptkeeperumia@gmail.com>
2013-05-01 20:15:22 +0500

    //Deleted Useless plugins which were giving Cheetah.jas error Added Ngui plugin

---
+++ Assets/NGUI/Scripts/Tweening/TweenAlpha.cs
@@ -0,0 +1,74 @@
+﻿//----------------------------------------------
+//            NGUI: Next-Gen UI kit
+// Copyright © 2011-2012 Tasharen Entertainment
+//----------------------------------------------
+
+using UnityEngine;
+
+/// <summary>
+/// Tween the object's alpha.
+/// </summary>
+
+[AddComponentMenu("NGUI/Tween/Alpha")]
+public class TweenAlpha : UITweener
+{
+	public float from = 1f;
+	public float to = 1f;
+
+	Transform mTrans;
+	UIWidget mWidget;
+	UIPanelAlpha mPanelAlpha;
+
+	/// <summary>
+	/// Current alpha.
+	/// </summary>
+
+	public float alpha
+	{
+		get
+		{
+			if (mWidget != null) return mWidget.alpha;
+			if (mPanelAlpha != null) return mPanelAlpha.alpha;
+			return 0f;
+		}
+		set
+		{
+			if (mWidget != null) mWidget.alpha = value;
+			else if (mPanelAlpha != null) mPanelAlpha.alpha = value;
+		}
+	}
+
+	/// <summary>
+	/// Find all needed components.
+	/// </summary>
+
+	void Awake ()
+	{
+		mPanelAlpha = GetComponent<UIPanelAlpha>();
+		if (mPanelAlpha == null) mWidget = GetComponentInChildren<UIWidget>();
+	}
+
+	/// <summary>
+	/// Interpolate and update the alpha.
+	/// </summary>
+
+	override protected void OnUpdate (float factor, bool isFinished) { alpha = Mathf.Lerp(from, to, factor); }
+
+	/// <summary>
+	/// Start the tweening operation.
+	/// </summary>
+
+	static public TweenAlpha Begin (GameObject go, float duration, float alpha)
+	{
+		TweenAlpha comp = UITweener.Begin<TweenAlpha>(go, duration);
+		comp.from = comp.alpha;
+		comp.to = alpha;
+
+		if (duration <= 0f)
+		{
+			comp.Sample(1f, true);
+			comp.enabled = false;
+		}
+		return comp;
+	}
+}
---
+++ Assets/NGUI/Scripts/UI/UIRoot.cs
@@ -0,0 +1,165 @@
+﻿//----------------------------------------------
+//            NGUI: Next-Gen UI kit
+// Copyright © 2011-2012 Tasharen Entertainment
+//----------------------------------------------
+
+using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// This is a script used to keep the game object scaled to 2/(Screen.height).
+/// If you use it, be sure to NOT use UIOrthoCamera at the same time.
+/// </summary>
+
+[ExecuteInEditMode]
+[AddComponentMenu("NGUI/UI/Root")]
+public class UIRoot : MonoBehaviour
+{
+	static List<UIRoot> mRoots = new List<UIRoot>();
+
+	/// <summary>
+	/// List of all UIRoots present in the scene.
+	/// </summary>
+
+	static public List<UIRoot> list { get { return mRoots; } }
+
+	Transform mTrans;
+
+	/// <summary>
+	/// If set to 'true', the UIRoot will be scaled automatically as the resolution changes, and will keep the UI under it
+	/// pixel-perfect. If set to 'false', 'manualHeight' is used instead, and the UI will scale proportionally to screen's height,
+	/// always remaining the same relative size regardless of the resolution changes.
+	/// </summary>
+
+	public bool automatic = true;
+
+	/// <summary>
+	/// Height of the screen when 'automatic' is turned off.
+	/// </summary>
+
+	public int manualHeight = 800;
+
+	/// <summary>
+	/// If the screen height goes below this value, it will be as if 'automatic' is turned off with the 'manualHeight' set to this value.
+	/// </summary>
+
+	public int minimumHeight = 320;
+
+	/// <summary>
+	/// If the screen height goes above this value, it will be as if 'automatic' is turned off with the 'manualHeight' set to this value.
+	/// </summary>
+
+	public int maximumHeight = 1536;
+
+	/// <summary>
+	/// UI Root's active height, based on the size of the screen.
+	/// </summary>
+
+	public int activeHeight
+	{
+		get
+		{
+			int height = Mathf.Max(2, Screen.height);
+
+			if (automatic)
+			{
+				if (height < minimumHeight) return minimumHeight;
+				if (height > maximumHeight) return maximumHeight;
+				return height;
+			}
+			return manualHeight;
+		}
+	}
+
+	/// <summary>
+	/// Pixel size adjustment. Most of the time it's at 1, unless 'automatic' is turned off.
+	/// </summary>
+
+	public float pixelSizeAdjustment
+	{
+		get
+		{
+			float height = Screen.height;
+
+			if (automatic)
+			{
+				if (height < minimumHeight) return minimumHeight / height;
+				if (height > maximumHeight) return maximumHeight / height;
+				return 1f;
+			}
+			return manualHeight / height;
+		}
+	}
+
+	void Awake () { mTrans = transform; mRoots.Add(this); }
+	void OnDestroy () { mRoots.Remove(this); }
+
+	void Start ()
+	{
+		UIOrthoCamera oc = GetComponentInChildren<UIOrthoCamera>();
+		
+		if (oc != null)
+		{
+			Debug.LogWarning("UIRoot should not be active at the same time as UIOrthoCamera. Disabling UIOrthoCamera.", oc);
+			Camera cam = oc.gameObject.GetComponent<Camera>();
+			oc.enabled = false;
+			if (cam != null) cam.orthographicSize = 1f;
+		}
+	}
+
+	void Update ()
+	{
+		if (mTrans != null)
+		{
+			float calcActiveHeight = activeHeight;
+
+			if (calcActiveHeight > 0f )
+			{
+				float size = 2f / calcActiveHeight;
+				
+				Vector3 ls = mTrans.localScale;
+	
+				if (!(Mathf.Abs(ls.x - size) <= float.Epsilon) ||
+					!(Mathf.Abs(ls.y - size) <= float.Epsilon) ||
+					!(Mathf.Abs(ls.z - size) <= float.Epsilon))
+				{
+					mTrans.localScale = new Vector3(size, size, size);
+				}
+			}
+		}
+	}
+
+	/// <summary>
+	/// Broadcast the specified message to the entire UI.
+	/// </summary>
+
+	static public void Broadcast (string funcName)
+	{
+		for (int i = 0, imax = mRoots.Count; i < imax; ++i)
+		{
+			UIRoot root = mRoots[i];
+			if (root != null) root.BroadcastMessage(funcName, SendMessageOptions.DontRequireReceiver);
+		}
+	}
+
+	/// <summary>
+	/// Broadcast the specified message to the entire UI.
+	/// </summary>
+
+	static public void Broadcast (string funcName, object param)
+	{
+		if (param == null)
+		{
+			// More on this: http://answers.unity3d.com/questions/55194/suggested-workaround-for-sendmessage-bug.html
+			Debug.LogError("SendMessage is bugged when you try to pass 'null' in the parameter field. It behaves as if no parameter was specified.");
+		}
+		else
+		{
+			for (int i = 0, imax = mRoots.Count; i < imax; ++i)
+			{
+				UIRoot root = mRoots[i];
+				if (root != null) root.BroadcastMessage(funcName, param, SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+}
